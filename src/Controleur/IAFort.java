c7d84ab942e4ecd66fd816b83c670100ef28df82
jizong2020 <75494208+jizong2020@users.noreply.github.com>
2021-05-13 00:06:22 +0200

    la fonction mouvement

--- src/Controleur/IAFort.java
+++ src/Controleur/IAFort.java
@@ -4,7 +4,9 @@ import Modele.Jeu;
 import Structures.Mouvement;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class IAFort extends IA{
 
@@ -12,7 +14,7 @@ public class IAFort extends IA{
         super(j, joueur);
     }
 
-
+    /* Prends le jeu courant et encode la configuration de ce jeu */
     public HashMap<Byte, Byte> configuration(){
         HashMap<Byte, Byte> resultat = new HashMap<>();
         for (int i = 0; i < 9; i++){        // modifier 10 après taille est pulblic dans jeu
@@ -27,6 +29,69 @@ public class IAFort extends IA{
             }
         }
         return resultat;
+    }
+
+    /* Evaluer la note d'une feuille */
+    public int evaluation(HashMap<Byte, Byte> configuration){
+        int nombre0 = 0;
+        int nombre1 = 0;
+        for(byte value : configuration.values()) {
+            int couleur = value >> 7;
+            if (couleur == 0)
+                nombre0++;
+            else
+                nombre1++;
+        }
+        if (nombre0 == nombre1)
+            return 0;
+        else{
+            if (joueur == 0)
+                return nombre0 > nombre1 ? 1 : -1;
+            else
+                return nombre1 > nombre0 ? 1 : -1;
+        }
+    }
+
+    /* A partir d'une configuration courante, retourne toutes les configurations possibles pour en une étape*/
+    public ArrayList<HashMap<Byte, Byte>> configurationSuivants(HashMap<Byte, Byte> configuration){
+        ArrayList<HashMap<Byte, Byte>> resultat = new ArrayList<>();
+        //TOTO
+        return resultat;
+    }
+
+
+    /* A partir d'une configuration qu'on a trouver pour IA, retourner la case de depart et la case d'arrivée */
+    public Mouvement configurationVersMouvement(HashMap<Byte, Byte> configuration, HashMap<Byte, Byte> suivant){
+        Iterator<HashMap.Entry<Byte, Byte>> iterator;
+        iterator = configuration.entrySet().iterator();
+        byte keyDepart = 0;
+        byte keyArrivee = 0;
+        while (iterator.hasNext()) {
+            HashMap.Entry<Byte, Byte> entry = iterator.next();
+            keyDepart = entry.getKey();
+            if (!suivant.containsKey(keyDepart))
+                break;
+        }
+        int hDepart = super.hashCodeVerH(keyDepart);
+        int lDepart = super.hashCodeVerL(keyDepart);
+        Point depart = new Point(hDepart, lDepart);
+        iterator = configuration.entrySet().iterator();
+        int valueDepart;
+        int valueArrivee;
+        while (iterator.hasNext()) {
+            HashMap.Entry<Byte, Byte> entry = iterator.next();
+            keyArrivee = entry.getKey();
+            valueDepart = entry.getValue();
+            if (keyArrivee != keyDepart) {
+                valueArrivee = suivant.get(keyDepart);
+                if (valueDepart != valueArrivee)
+                    break;
+            }
+        }
+        int hArrivee = super.hashCodeVerH(keyArrivee);
+        int lArrivee = super.hashCodeVerL(keyArrivee);
+        Point arrivee = new Point(hArrivee, lArrivee);
+        return new Mouvement(depart, arrivee);
     }
 
     @Override

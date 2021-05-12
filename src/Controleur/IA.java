e27489a91ec7557f049d63ebd4d3afcbb1331823
jizong2020 <75494208+jizong2020@users.noreply.github.com>
2021-05-12 22:36:50 +0200

    IA Aléatoire

--- src/Controleur/IA.java
+++ src/Controleur/IA.java
@@ -4,15 +4,18 @@ import Modele.Jeu;
 import Structures.Mouvement;
 
 
+import java.awt.*;
 import java.util.ArrayList;
 
 abstract class IA {
-    public Jeu jeu;
-    public ArrayList<Integer> voisinsAccessible;
+    Jeu jeu;
+    boolean joueur;
+    ArrayList<Point> casePeutBouger;
 
-    public IA (Jeu j) {
+    public IA (Jeu j, boolean joueur) {
         jeu = j;
-        voisinsAccessible = new ArrayList<>();
+        this.joueur = joueur;
+        casePeutBouger = new ArrayList<>();
     }
 
     public byte hashCode(int l, int h){
@@ -24,56 +27,60 @@ abstract class IA {
     }
 
     public int hashCodeVerH(byte code){
-        return (int) (code & 0XF);
+        return (code & 0XF);
     }
 
+    public ArrayList<Point> trouveVosinsAccessible(Point depart){
+        ArrayList<Point> voisinsAccessible = new ArrayList<>();
+        int h = depart.x;
+        int l = depart.y;
 
-    public boolean peutEtreBouger(int hauteurDepart, int hauteurArrivee){
-        if (hauteurDepart + hauteurArrivee <= 5)
-            return true;
-        else
-            return false;
-    }
-
-
-    public void trouveAccessible(int l, int h){
-/*      Attendre le class jeu
-        int hauteurDepart = jeu.;
-        int hauteurArrivee;
-       if (h-1 >= 0){        //(l, h-1)
-
+        if (h-1 >= 0){        //(h-1, l)
+            if (jeu.estMouvementPossible(depart, new Point(h-1, l)))
+                voisinsAccessible.add(new Point(h-1, l));
         }
-        if (h+1 <= ){         //(l, h+1)
-
+        if (h+1 <= 9){         //(h+1, l)
+            if (jeu.estMouvementPossible(depart, new Point(h+1, l)))
+                voisinsAccessible.add(new Point(h+1, l));
         }
-
         if (l-1 >= 0){
-            if (){            //(l-1, h)
-
+            if (jeu.estMouvementPossible(depart, new Point(h, l-1)))     //(h, l-1)
+                voisinsAccessible.add(new Point(h, l-1));
+            if (h-1 >= 0){    //(h-1, l-1)
+                if (jeu.estMouvementPossible(depart, new Point(h-1, l-1)))     //(h-1, l-1)
+                    voisinsAccessible.add(new Point(h-1, l-1));
             }
-            if (h-1 >= 0){    //(l-1, h-1)
-
-            }
-            if (h+1 < jeu.){  //(l-1, h+1)
-
+            if (h+1 <= 9){  //(h+1, l-1)
+                if (jeu.estMouvementPossible(depart, new Point(h+1, l-1)))     //(h+1, l-1)
+                    voisinsAccessible.add(new Point(h-1, l-1));
             }
         }
-        if (l+1 < ){
-            if (){            //(l+1, h)
-
+        if (l+1 <= 9){
+            if (jeu.estMouvementPossible(depart, new Point(h, l+1)))     //(h, l+1)
+                voisinsAccessible.add(new Point(h, l+1));
+            if (h-1 >= 0){    //(h-1, l+1)
+                if (jeu.estMouvementPossible(depart, new Point(h-1, l+1)))     //(h, l+1)
+                    voisinsAccessible.add(new Point(h-1, l+1));
             }
-            if (h-1 >= 0){    //(l+1, h-1)
-
+            if (h+1 <= 9){  //(h+1, l+1)
+                if (jeu.estMouvementPossible(depart, new Point(h+1, l+1)))     //(h, l+1)
+                    voisinsAccessible.add(new Point(h+1, l+1));
             }
-            if (h+1 < jeu.){  //(l+1, h+1)
-
-            }
-
         }
-*/
+        return voisinsAccessible;
     }
 
-
+    public void trouveCasePeutBouger(){
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                ArrayList<Point> vosinsAccessible = trouveVosinsAccessible(new Point(i, j));
+                if (vosinsAccessible != null){
+                    if (vosinsAccessible.size() != 0)
+                        casePeutBouger.add(new Point(i, j));
+                }
+            }
+        }
+    }
 
     public abstract Mouvement joue();
 }
--- src/Controleur/IAAleatoire.java
+++ src/Controleur/IAAleatoire.java
@@ -3,19 +3,28 @@ package Controleur;
 import Structures.Mouvement;
 
 import Modele.Jeu;
+
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class IAAleatoire extends IA{
     Random r1, r2;
 
-    public IAAleatoire(Jeu j) {
-        super(j);
+    public IAAleatoire(Jeu j, boolean joueur) {
+        super(j, joueur);
         r1 = new Random();
         r2 = new Random();
     }
 
     @Override
     public Mouvement joue() {
-        return null;
+        super.trouveCasePeutBouger();
+        int d = r1.nextInt(casePeutBouger.size());
+        Point depart = casePeutBouger.get(d);
+        ArrayList<Point> voisinsAccessible = super.trouveVosinsAccessible(depart);
+        int a = r2.nextInt(voisinsAccessible.size());
+        Point arrivee = voisinsAccessible.get(a);
+        return new Mouvement(depart, arrivee);
     }
 }

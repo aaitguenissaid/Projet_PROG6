1d5866955dd7d35237f6a5118ca9102d1f00b6e1
jizong2020 <75494208+jizong2020@users.noreply.github.com>
2021-05-12 19:27:21 +0200

    ajouter des class peutEtreBouger

--- src/Controleur/IA.java
+++ src/Controleur/IA.java
@@ -3,7 +3,7 @@ package Controleur;
 import Modele.Jeu;
 import Structures.Mouvement;
 
-import java.awt.*;
+
 import java.util.ArrayList;
 
 abstract class IA {
@@ -14,6 +14,65 @@ abstract class IA {
         jeu = j;
         voisinsAccessible = new ArrayList<>();
     }
+
+    public int hashCode(int l, int h){
+        return l*10 + h;
+    }
+
+    public int hashCodeVerL(int code){
+        return code/10;
+    }
+
+    public int hashCodeVerH(int code){
+        return code%10;
+    }
+
+
+    public boolean peutEtreBouger(int hauteurDepart, int hauteurArrivee){
+        if (hauteurDepart + hauteurArrivee <= 5)
+            return true;
+        else
+            return false;
+    }
+
+
+    public void trouveAccessible(int l, int h){
+/*      Attendre le class jeu
+        int hauteurDepart = jeu.;
+        int hauteurArrivee;
+       if (h-1 >= 0){        //(l, h-1)
+
+        }
+        if (h+1 <= ){         //(l, h+1)
+
+        }
+
+        if (l-1 >= 0){
+            if (){            //(l-1, h)
+
+            }
+            if (h-1 >= 0){    //(l-1, h-1)
+
+            }
+            if (h+1 < jeu.){  //(l-1, h+1)
+
+            }
+        }
+        if (l+1 < ){
+            if (){            //(l+1, h)
+
+            }
+            if (h-1 >= 0){    //(l+1, h-1)
+
+            }
+            if (h+1 < jeu.){  //(l+1, h+1)
+
+            }
+
+        }
+*/
+    }
+
 
 
     public abstract Mouvement joue();

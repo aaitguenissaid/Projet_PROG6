b8aa594421afd53064dfe1187d93760bcb6fabba
Sidorets Kirill <Sidorets.Kirill97@gmail.com>
2021-05-25 22:41:56 +0200

    FAP fix

---
+++ src/Structures/FAP.java
@@ -0,0 +1,17 @@
+package Structures;
+
+
+public abstract class FAP<Bob> {
+    Sequence<Bob> s;
+
+    abstract void insere(Bob element);
+    Bob extrait() {
+        return s.extraitTete();
+    }
+    boolean estVide() {
+        return s.estVide();
+    }
+    public Iterateur iterateur(){
+        return s.iterateur();
+    }
+}

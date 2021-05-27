da92523b5d8e6cac25bd3a88e1e3ce067e57fb46
Sidorets Kirill <Sidorets.Kirill97@gmail.com>
2021-05-27 03:45:53 +0200

    Reagles fix

--- src/Vue/Regles.java
+++ src/Vue/Regles.java
@@ -9,6 +9,7 @@ import java.io.File;
 public class Regles extends javax.swing.JPanel {
     CollecteurEvenements cc;
     PaletteDeCouleurs palette;
+    String fontColor;
     public Regles(CollecteurEvenements ctrl) {
         cc=ctrl;
         palette = cc.getPalette();
@@ -47,40 +48,36 @@ public class Regles extends javax.swing.JPanel {
         jPanel8.setLayout(new javax.swing.BoxLayout(jPanel8, javax.swing.BoxLayout.PAGE_AXIS));
         jPanel8.add(addImageToPanel("12"));
         jPanel8.add(addImageToPanel("1"));
-        jPanel8.add(addTextToPanel("<html><h1>Préparation du jeu</h1>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Disposer sur les trous les 48 pions en alternant les couleurs.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><h1 style='color:"+fontColor+";padding:5px;' >Préparation du jeu</h1></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Disposer sur les trous les 48 pions en alternant les couleurs.</p></html>"));
         jPanel8.add(addImageToPanel("2"));
-        jPanel8.add(addTextToPanel("<html><h1>Régles</h1>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Chaque joueur choisit sa couleur.</p>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Chaque joueur a le droit de jouer avec tous les pions, y compris ceux de l'adversaire.</p>"+
-                "<h1>But du jeu</h1>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Posséder un maximum de tours dont le pion supérieur est de la couleur choisie.</p>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Une tour est constituée de 1 à 5 pions. Jamais plus.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><h1 style='color:"+fontColor+";padding:5px;'>Régles</h1></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Chaque joueur choisit sa couleur.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Chaque joueur a le droit de jouer avec tous les pions, y compris ceux de l'adversaire.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><h1 style='color:"+fontColor+";padding:5px;'>But du jeu</h1></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Posséder un maximum de tours dont le pion supérieur est de la couleur choisie.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Une tour est constituée de 1 à 5 pions. Jamais plus.</p></html>"));
         jPanel8.add(addImageToPanel("3"));
-        jPanel8.add(addTextToPanel("<html><p style=\"text-align: justify;  text-justify: inter-word;\">Un joueur est le propriétaire d’une tour lorsqu’un pion de sa couleur en occupe le sommet.</p>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Un pion isolé est également considéré comme une tour.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Un joueur est le propriétaire d’une tour lorsqu’un pion de sa couleur en occupe le sommet.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Un pion isolé est également considéré comme une tour.</p></html>"));
         jPanel8.add(addImageToPanel("4"));
-        jPanel8.add(addTextToPanel("<html><h1>Déplacements</h1>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">A tour de rôle, chaque joueur effectue un seul déplacement à partir de n‘importe quel pion ou pile de pions. Chacun est donc libre de jouer où bon lui semble.</p>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Un déplacement consiste à empiler le ou les pions sur un emplacement directement voisin (diagonale = ok) déjà occupé par un ou plusieurs pions (jamais sur un trou vide).</p></html>"));
+        jPanel8.add(addTextToPanel("<html><h1 style='color:"+fontColor+";padding:5px;'>Déplacements</h1></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">A tour de rôle, chaque joueur effectue un seul déplacement à partir de n‘importe quel pion ou pile de pions. Chacun est donc libre de jouer où bon lui semble.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Un déplacement consiste à empiler le ou les pions sur un emplacement directement voisin (diagonale = ok) déjà occupé par un ou plusieurs pions (jamais sur un trou vide).</p></html>"));
         jPanel8.add(addImageToPanel("5"));
-        jPanel8.add(addTextToPanel("<html><p>=> Chaque coup joué équivaut donc à libérer un emplacement.</p>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">=> Il est interdit de passer au-dessus d’un trou vide.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">=> Chaque coup joué équivaut donc à libérer un emplacement.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">=> Il est interdit de passer au-dessus d’un trou vide.</p></html>"));
         jPanel8.add(addImageToPanel("6"));
-        jPanel8.add(addTextToPanel("<html><p style=\"text-align: justify;  text-justify: inter-word;\">On déplace obligatoirement tous les pions situés sur un emplacement (de 1 a 4. Une tour de 5 pions ne se déplace pas).</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">On déplace obligatoirement tous les pions situés sur un emplacement (de 1 a 4. Une tour de 5 pions ne se déplace pas).</p></html>"));
         jPanel8.add(addImageToPanel("7"));
-        jPanel8.add(addTextToPanel("<html><p style=\"text-align: justify;  text-justify: inter-word;\">Une pile de pions ne peut qu’augmenter, jamais diminuer.</p>" +
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">Un pion ou une tour, isolés de tous les côtés, ne pourront donc plus bouger ni changer de propriétaire.</p>" +
-                "<h1>Fin de la partie</h1>"+
-                "<p style=\"text-align: justify;  text-justify: inter-word;\">La partie se poursuit tant qu’on peut effectuer des empilements. Le jeu se termine lorsque plus aucun déplacement n’est possible.\n" +
-                "Le plateau n’est donc plus constitué que de tours ou de pions isolés.\n" +
-                "Seul le dernier étage compte.\n" +
-                "\n" +
-                "=> Le vainqueur est le joueur totalisant le plus grand nombre de pions isolés ou de sommets de tours de sa couleur.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Une pile de pions ne peut qu’augmenter, jamais diminuer.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">Un pion ou une tour, isolés de tous les côtés, ne pourront donc plus bouger ni changer de propriétaire.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><h1 style='color:"+fontColor+";padding:5px;'>Fin de la partie</h1></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">La partie se poursuit tant qu’on peut effectuer des empilements. Le jeu se termine lorsque plus aucun déplacement n’est possible. Seul le dernier étage compte.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">=> Le vainqueur est le joueur totalisant le plus grand nombre de pions isolés ou de sommets de tours de sa couleur.</p></html>"));
         jPanel8.add(addImageToPanel("8"));
-        jPanel8.add(addTextToPanel("<html><p>= Qu’une tour compte 1,2... ou 5 pions, elle vaut toujours UN point.\n" +
-                "\n" +
-                "En cas d’égalité, c'est celui qui a le plus de tours de 5 pions (avec le pion de sa couleur au sommet) qui a gagné.\n</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">= Qu’une tour compte 1,2... ou 5 pions, elle vaut toujours UN point.</p></html>"));
+        jPanel8.add(addTextToPanel("<html><p style=\"color:"+fontColor+";padding:10px; text-align: justify;  text-justify: inter-word;\">En cas d’égalité, c'est celui qui a le plus de tours de 5 pions (avec le pion de sa couleur au sommet) qui a gagné.</p></html>"));
         jScrollPane5.setViewportView(jPanel8);
         jScrollPane5.getVerticalScrollBar().setUnitIncrement(16);
         add(jScrollPane5);
@@ -106,10 +103,10 @@ public class Regles extends javax.swing.JPanel {
         panel.setBackground(palette.Couleur1);
         panel.setLayout(new java.awt.GridLayout());
         JLabel label = new JLabel();
-        label.setPreferredSize(new Dimension(label.getPreferredSize().height,getPreferredSize().width));
         label.setBackground(palette.Couleur1);
         label.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         label.setText(text);
+        label.setPreferredSize(new Dimension(getPreferredSize().width,label.getPreferredSize().height));
         panel.add(label);
         return panel;
     }
@@ -118,6 +115,7 @@ public class Regles extends javax.swing.JPanel {
         jButton1.setBackground(palette.Couleur1);
         jLabel1.setBackground(palette.Couleur1);
         jPanel8.setBackground(palette.Couleur1);
+        fontColor = String.format("#%02x%02x%02x", palette.Couleur3.getRed(), palette.Couleur3.getGreen(), palette.Couleur3.getBlue());
     }
     private javax.swing.Box.Filler filler1;
     private javax.swing.Box.Filler filler2;
